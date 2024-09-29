4a2683fd2996306f842ba62694f39160dfc45f0a
YEAHx4 <developerstarlight@gmail.com>
2024-09-29 10:43:32 +0900

    User model

---
+++ src/main/kotlin/io/yeahx4/model/User.kt
@@ -0,0 +1,12 @@
+package io.yeahx4.model
+
+data class User(
+    val userId: String,
+    val password: String,
+    val name: String,
+    val email: String
+) {
+    override fun toString(): String {
+        return "User(userId='$userId', password='$password', name='$name', email='$email')"
+    }
+}
